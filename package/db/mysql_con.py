836ed6fc120f39e4df9677e3bf062b6f1e947b4d
paul <lipan@souche.com>
2016-12-03 00:12:26 +0800

    [WN] update create tables sql

--- package/db/mysql_con.py
+++ package/db/mysql_con.py
@@ -1,9 +1,31 @@
 import MySQLdb
 
-if __name__ == '__main__':
-    db = MySQLdb.connect("localhost","root","","pauldatas")
+
+def printDBVersion(db):
     cursor = db.cursor()
     cursor.execute("SELECT VERSION()")
     data = cursor.fetchone()
     print 'DataBase Version is %s' % data
     cursor.close()
+
+
+if __name__ == '__main__':
+    db = MySQLdb.connect("localhost","root","","pauldatas")
+    printDBVersion(db)
+
+
+def createTables(db):
+    sql = """CREATE TABLE STUDENT (
+         FIRST_NAME  CHAR(20) NOT NULL,
+         LAST_NAME  CHAR(20),
+         AGE INT,
+         SEX CHAR(1),
+         INCOME FLOAT )"""
+    cursor = db.cursor()
+    try:
+        cursor.execute(sql)
+        db.commit()
+    except:
+        db.rollback()
+    finally:
+        cursor.close()
