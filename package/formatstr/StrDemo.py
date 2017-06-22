9e14bbfb200f691ee0667f7ba8e01e126c4c3d4f
Paullee <javaclee@gmail.com>
2017-06-22 22:17:33 +0800

    [pref] 拆分整理函数

---
+++ package/formatstr/StrDemo.py
@@ -0,0 +1,18 @@
+index = 0
+info = '0'
+#  The '+' operator can concatenate two strings
+print(str(index) + info)
+
+# raw
+nraw = 'this\nhhhhh\thhhhh'
+print(nraw)
+
+raw = r'this\nhhhhh\thhhhh'
+print(raw)
+
+
+
+# if
+
+# The "zero" values all count as false: None, 0, empty string, empty list, empty dictionary.
+# There is also a Boolean type with two values: True and False (converted to an int, these are 1 and 0).
